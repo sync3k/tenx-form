3301f6a665143e9740f86e18d4277652d7bda0a1
In-Ho Yi <chajath@gmail.com>
2017-10-14 12:47:37 -0700

    Render form submissions

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import {createStore, compose, Store} from 'redux';
-import {diff} from 'deep-diff';
-import {enhancer, actions} from 'sync3k-client';
+import { createStore, compose, Store } from 'redux';
+import { diff } from 'deep-diff';
+import { enhancer, actions } from 'sync3k-client';
 
 type TenxFormState = {
-  submitted: [{[key: string]: string}],
-  current: {[key: string]: string},
+  submitted: [{ [key: string]: string }],
+  current: { [key: string]: string },
 }
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 class TenxForm extends HTMLElement {
-  private state: Store<TenxFormState> = createStore((state = {current: {}, submitted: [] as [{[key: string]: string}]}, action) => {
+  private state: Store<TenxFormState> = createStore((state = { current: {}, submitted: [] as [{ [key: string]: string }] }, action) => {
     switch (action.type) {
       case 'UPDATE':
         return {
@@ -22,7 +22,7 @@ class TenxForm extends HTMLElement {
       case 'SUBMIT':
         return {
           ...state,
-          submitted: [state.current, ...state.submitted]
+          submitted: [...state.submitted, state.current]
         }
     }
 
@@ -36,9 +36,9 @@ class TenxForm extends HTMLElement {
   connectedCallback() {
     this.state.dispatch(
       actions.initializeSync(
-        this.attributes.getNamedItem('sync3k-base-url').value, 
+        this.attributes.getNamedItem('sync3k-base-url').value,
         this.attributes.getNamedItem('sync3k-topic').value,
-        '', 
+        '',
         false));
   }
 
@@ -48,43 +48,62 @@ class TenxForm extends HTMLElement {
       mutations.forEach((me) => {
         me.addedNodes.forEach((node) => {
           if (node instanceof HTMLInputElement) {
-            switch(node.type) {
+            switch (node.type) {
               case "submit": {
                 node.addEventListener("click", (e) => {
                   e.preventDefault();
-                  this.state.dispatch({type: 'SUBMIT', value: this.state.getState().current});
+                  this.state.dispatch({ type: 'SUBMIT', value: this.state.getState().current });
                 })
                 break;
               }
               default: {
                 node.addEventListener("blur", () => {
-                  this.state.dispatch({type: 'UPDATE', id: node.id, value: node.value});
+                  this.state.dispatch({ type: 'UPDATE', id: node.id, value: node.value });
                 });
               }
             }
           }
           if (node instanceof HTMLTextAreaElement) {
             node.addEventListener("blur", () => {
-              this.state.dispatch({type: 'UPDATE', id: node.id, value: node.value});
+              this.state.dispatch({ type: 'UPDATE', id: node.id, value: node.value });
             });
           }
         });
       })
     });
-    observer.observe(this, {childList: true});
+    observer.observe(this, { childList: true });
 
-    let prevState: TenxFormState = {current: {}, submitted: [] as [{[key: string]: string}]};
+    let prevState: TenxFormState = { current: {}, submitted: [] as [{ [key: string]: string }] };
     this.state.subscribe(() => {
-      const diffs = diff(prevState.current, this.state.getState().current);
-      for (const diff of diffs || []) {
+      const currentDiffs = diff(prevState.current, this.state.getState().current);
+      for (const diff of currentDiffs || []) {
         const target = this.querySelector(`#${diff.path[0]}`) as HTMLTextAreaElement | HTMLInputElement;
         if (target !== document.activeElement) {
           target.value = diff.rhs;
         }
       }
+
+      const submittedDiffs = diff(prevState.submitted, this.state.getState().submitted);
+      for (const diff of submittedDiffs || []) {
+        const newContent = renderSubmission(document.querySelector(`#${this.attributes.getNamedItem('result-template').value}`) as HTMLTemplateElement, diff.item.rhs);
+
+        const resultDiv = document.querySelector(`#${this.attributes.getNamedItem('result-div').value}`);
+        resultDiv.insertBefore(newContent, resultDiv.firstChild);
+      }
+
       prevState = this.state.getState();
     });
   }
+}
+
+const renderSubmission = (template: HTMLTemplateElement, submitted: any) => {
+  // Deep-clone document fragment from template.
+  const content = template.content.cloneNode(true) as DocumentFragment;
+  for (const formKey of Object.keys(submitted)) {
+    (content.querySelector(`[tenx-name="${formKey}"]`) as HTMLElement).innerText = submitted[formKey];
+  }
+
+  return content;
 }
 
 customElements.define('tenx-form', TenxForm);
